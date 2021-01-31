968bd6499269efd7e8e7ef26b68948d3ff9fd27f
Marlon Buella <marlon.b.buella@gmail.com>
2021-02-01 01:20:01 +0800

    Add Tailwind CSS

--- src/components/hello-world.js
+++ src/components/hello-world.js
@@ -1,7 +1,9 @@
 import React from "react"
 
 const HelloWorld = () => (
-  <div>Hello world!</div>
+  <div>
+    <h1>Hello world!</h1>
+  </div>
 )
 
 export default HelloWorld
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,10 @@
 import React from "react"
 import HelloWorld from "./../components/hello-world"
 
+// import CSS
+import "./../styles/tw.css"
+import "./../styles/main.css"
+
 const Home = () => (
     <main>
       <HelloWorld />
